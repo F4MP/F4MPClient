e8e0ce4989607ede70e45044b30888d7881c32e8
alin <alinoctavian@protonmail.com>
2020-08-18 13:45:03 +0300

    Added the right entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <windows.h>
 #include <iostream>
 
-int main(void){
-    std::cout << "Hello" << std::endl;
-    return 1;
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,  DWORD fdwReason, LPVOID lpReserved ){
+
+    switch(fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+         // Initialize once for each new process.
+         // Return FALSE to fail DLL load.
+            break;
+
+        case DLL_THREAD_ATTACH:
+         // Do thread-specific initialization.
+            break;
+
+        case DLL_THREAD_DETACH:
+            
+            break;
+    }
+
+    return TRUE;
 }
