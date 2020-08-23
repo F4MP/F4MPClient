bf43502d81e87dca07e67246b6238438ad362646
alin <alinoctavian@protonmail.com>
2020-08-23 17:46:40 +0300

    Added correct namespace

--- src/Utils/VMTHook.h
+++ src/Utils/VMTHook.h
@@ -6,24 +6,32 @@
 #include <cstdint>
 #include <cstdlib>
 
-template <std::size_t VMTOffset>
-void PatchVMTMethod(uintptr_t base, uintptr_t dst) {
-    const uintptr_t method_offset = base + (VMTOffset * sizeof(uintptr_t));
+namespace Memory {
 
-    ReprotectScope<MemPerm::ReadWriteExecute> protection(method_offset, sizeof(uintptr_t));
-    PatchType<uintptr_t>(method_offset, dst);
-}
+    namespace VMT {
 
-template <typename T, std::size_t VMTOffset>
-T CreateVMTMethodDetour(uintptr_t base, uintptr_t dst) {
-    const uintptr_t method_offset = base + (VMTOffset * sizeof(uintptr_t));
+        template<std::size_t VMTOffset>
+        void PatchVMTMethod(uintptr_t base, uintptr_t dst) {
+            const uintptr_t method_offset = base + (VMTOffset * sizeof(uintptr_t));
 
-    ReprotectScope<MemPerm::ReadWriteExecute> protection(method_offset, sizeof(uintptr_t));
+            ReprotectScope <MemPerm::ReadWriteExecute> protection(method_offset, sizeof(uintptr_t));
+            PatchType<uintptr_t>(method_offset, dst);
+        }
 
-    auto original = ReadType<uintptr_t>(method_offset);
-    PatchType<uintptr_t>(method_offset, dst);
+        template<typename T, std::size_t VMTOffset>
+        T CreateVMTMethodDetour(uintptr_t base, uintptr_t dst) {
+            const uintptr_t method_offset = base + (VMTOffset * sizeof(uintptr_t));
 
-    return reinterpret_cast<T>(original);
+            ReprotectScope <MemPerm::ReadWriteExecute> protection(method_offset, sizeof(uintptr_t));
+
+            auto original = ReadType<uintptr_t>(method_offset);
+            PatchType<uintptr_t>(method_offset, dst);
+
+            return reinterpret_cast<T>(original);
+        }
+
+    }
+
 }
 
 #endif //F4MPCLIENT_VMTHOOK_H
